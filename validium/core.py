62d297f31288096a34961790146e4c609ab7ec4d
Jason Yung <json.yung@gmail.com>
2020-03-03 16:40:05 -0500

    rename parameter

--- validium/core.py
+++ validium/core.py
@@ -1,14 +1,14 @@
 class Validator:
 
-  def __init__(self, predicate, fail_msg=None):
+  def __init__(self, predicate, msg=None):
 
     assert callable(predicate), "the argument 'predicate' must be callable"
-    assert fail_msg is None or isinstance(fail_msg, str), "the argument 'fail_msg' must be None or an instance of str"
+    assert msg is None or isinstance(msg, str), "the argument 'msg' must be None or an instance of str"
 
     self.__dict__ = dict(
       predicate=predicate,
-      fail_msg=fail_msg,
+      msg=msg,
     )
 
   def validate(self, target):
-    assert self.predicate(target), self.fail_msg
+    assert self.predicate(target), self.msg
