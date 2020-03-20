2228218384d6a6aae27c1d48b27f1f5e3682abff
Jason Yung <json.yung@gmail.com>
2020-03-20 19:50:27 -0400

    drop support for Python 3.5
    + in favor of using f-strings

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     'Topic :: Utilities',
     'License :: OSI Approved :: MIT License', # Again, pick a license
     'Programming Language :: Python :: 3', # Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.5',
+    # 'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
