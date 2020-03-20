10914cf622b7b2ce559a8fbb981c9e1ae165f8d6
Jason Yung <json.yung@gmail.com>
2020-03-20 19:13:19 -0400

    bump semver and update reqs

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'validium', # How you named your package folder (MyLib)
   packages = ['validium'], # Chose the same as "name"
-  version = '0.2.0', # Start with a small number and increase it with every change you make
+  version = '0.2.1', # Start with a small number and increase it with every change you make
   license= 'MIT', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'a Python utility library for performing validations flexibly', # Give a short description about your library
   author = 'Jason Yung',
   author_email = 'json.yung@gmail.com',
   url = 'https://github.com/json2d/validium', # Provide either the link to your github or to your website
-  download_url = 'https://github.com/json2d/validium/archive/v0.2.0.tar.gz',
+  download_url = 'https://github.com/json2d/validium/archive/v0.2.1.tar.gz',
   keywords = ['validation', 'validator', 'functional'], # Keywords that define your package best
   install_requires= [],
   classifiers=[
